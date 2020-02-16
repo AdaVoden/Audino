4987fe4a2342af9785d5267fea57634df686bddb
Harlan Shaw <thatdamnpillow@gmail.com>
2020-02-16 15:26:59 -0700

    Parser is currently working for all supported filetypes.

--- src/main/java/Audino/MetadataParser.java
+++ src/main/java/Audino/MetadataParser.java
@@ -1,9 +1,18 @@
+package Audino;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+
+import org.apache.commons.io.FilenameUtils;
 
 import org.apache.tika.metadata.Metadata;
 import org.xml.sax.ContentHandler;
@@ -11,52 +20,59 @@ import org.apache.tika.exception.TikaException;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.mp3.Mp3Parser;
+import org.gagravarr.tika.FlacParser;
+import org.gagravarr.tika.VorbisParser;
+import org.apache.tika.parser.audio.AudioParser;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class MetadataParser {
-    public static ArrayList<String> parse(InputStream file, String extension) {
-        ArrayList<String> toReturn;
-        switch (extension){
-        case ".mp3":
-            toReturn = parseMP3(file);
-            break;
-        case ".flac":
-            toREturn = parseFLAC(file);
-            break;
-        case ".ogg":
-            toReturn = parseOGG(file);
-            break;
-        case ".wma":
-            toReturn = parseWMA(file);
-            break;
-        case ".wav":
-            toReturn = parseWAV(file);
-            break;
+    public static Metadata parseAudio(String file) {
+        try {
+            InputStream input = AudioSystem.getAudioInputStream(new File(file).getAbsoluteFile());
+            //InputStream input = new AudioInputStream(new File(file));
+            String extension = FilenameUtils.getExtension(file);
+            Metadata toReturn = new Metadata();
+            ContentHandler handler = new DefaultHandler();
+            ParseContext parseCont = new ParseContext();
+            Parser parser;
+            switch (extension){
+            case "mp3":
+                parser = new Mp3Parser();
+                break;
+            case "flac":
+                parser = new FlacParser();
+                break;
+            case "ogg":
+                parser = new VorbisParser();
+                break;
+            case "wma":
+                parser = new AudioParser();
+                break;
+            case "wav":
+                parser = new AudioParser();
+                break;
+            default:
+                parser = new AudioParser();
+            }
+            parser.parse(input, handler, toReturn, parseCont);
+            return toReturn;
         }
-    }
-
-    public static ArrayList<String> parseMP3(InputStream file){
+        catch (IOException e){
+            //TODO Unreachable?
+            e.printStackTrace();
+        }
+        catch (SAXException e){
+            //TODO
+            e.printStackTrace();
+        }
+        catch (TikaException e){
+            //TODO
+            e.printStackTrace();
+        }
+        catch (UnsupportedAudioFileException e){
+            e.printStackTrace();
+        }
         return null;
     }
-    public static ArrayList<String> parseFLAC(InputStream file){
-        return null;
-
-    }
-
-    public static ArrayList<String> parseOGG(InputStream file) {
-        return null;
-
-    }
-
-    public static ArrayList<String> parseWMA(InputStream file) {
-        return null;
-
-    }
-
-    public static ArrayList<String> parseWAV(InputStream file) {
-        return null;
-
-    }
-
 }
--- src/main/java/Audino/Track.java
+++ src/main/java/Audino/Track.java
@@ -1,43 +1,22 @@
 package Audino;
 
-import java.io.IOException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.parser.mp3.Mp3Parser;
-import org.gagravarr.tika.FlacParser;
 import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.ParseContext;
-import org.apache.tika.parser.Parser;
-import org.apache.tika.parser.audio.AudioParser;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 
 
 public class Track {
-    private String name;
+    private String title;
     private String album;
     private String artist;
     private double duration;
     private String fileLocation;
 
-    public Track(String fileLocation) throws IOException {
-        int locOfFileType = fileLocation.indexOf(".");
-        String fileType = fileLocation.substring(locOfFileType);
-        try {
-            InputStream file = new FileInputStream(new File(fileLocation));
-            ArrayList<String> metadata = MetadataParser.parse(file, fileType);
-        }
-        catch (FileNotFoundException e){
-            // Do something. Not implemented.
-        }
-
+    public Track(String fileLocation) {
+        Metadata metadata = MetadataParser.parseAudio(fileLocation);
+        this.title = metadata.get("title");
+        this.album = metadata.get("album");
+        this.artist = metadata.get("xmpDM:artist");
+        this.duration = Double.parseDouble(metadata.get("xmpDM:duration"));
     }
 }
