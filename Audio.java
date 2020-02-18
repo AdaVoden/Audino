99e741caf79249511a43a7ee4f6d3d3e30289009
ryanmchale <ryanmchale@192.168.1.71>
2020-02-17 18:30:09 -0700

    Pause/Play button worked out. Appropriate error messages for if
    already playing or already paused.

--- Audio.java
+++ Audio.java
@@ -1,13 +1,17 @@
+/*
+/Users/ryanmchale/desktop/crazylungs16.wav
+ */
 
 import java.io.*;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import java.util.Scanner;
-public class Audio {
-	
+public class Audio 
+{
 	  public static Clip playSound(String filename) throws Exception
 	  {
 		  {			  
@@ -30,7 +34,6 @@ public class Audio {
 			AudioInputStream stream = AudioSystem.getAudioInputStream(new File(filename).getAbsoluteFile());
 
 			Clip clip = AudioSystem.getClip();
-			
 			clip.open(stream);
 			clip.loop(Clip.LOOP_CONTINUOUSLY);
 			clip.setMicrosecondPosition(startTime);
@@ -40,43 +43,17 @@ public class Audio {
 	  }
 	  }
 	  
-
-	  
-	  public static Clip pause(Clip clip,long startTime, String filename) throws Exception
-	  {
-		  clip.stop();
-		  Scanner scan = new Scanner(System.in);
-		  String status;
-		  boolean pause = true;
-		  while(pause = true)
-		  {
-			  System.out.println("To resume playback, type \"play\". ");
-			  status = scan.next();
-			  if(status.contains("play"))
-			  {
-				  pause = false;
-				  return playSoundAtTime(filename,startTime);
-			  }
-			  else
-			  {
-				  System.out.println("Invalid input. ");
-			  }
-		  }
-		  
-		  return clip;
-		  
-	  }
-	  
-	  
+  
+ 
 	 
 	  public static void main(String[] args) throws Exception
 	  {
 			  
 			 boolean run = true;
-			 boolean pause;
+			 boolean pause = false;
 			 String status;
 				 
-			 long setTime; 
+			 long setTime = 0; 
 			  
 			 Scanner file = new Scanner(System.in);
 			 Scanner running = new Scanner(System.in);
@@ -88,32 +65,60 @@ public class Audio {
 			 
 			 while(run == true)
 			 {
-				 System.out.println("To pause playback, type \"pause\"\r\nTo stop playback, type \"stop\". ");
+				 if(pause == false)	System.out.println("To pause playback, type \"pause\".\r\nTo stop playback, type \"stop\". ");
+				 if(pause == true)	System.out.println("To resume playback, type \"play\".\r\nTo stop playback, type \"stop\". ");
+
 				 status = running.next();
-				 
-				 if(status.contentEquals("pause"))
+				 if(pause == true)
 				 {
-					 setTime = currentClip.getMicrosecondPosition();
-					 //
-					 while(currentClip.getMicrosecondLength() < setTime )
-					 {
-						 setTime = setTime - currentClip.getMicrosecondLength();
-					 }
-					 currentClip = pause(currentClip,setTime,fileName);
-					 
+					 if(status.contentEquals("pause")) System.out.println("Already paused...");
 				 }
-				 else if(status.contentEquals("stop"))
+				 
+				 if(pause == false)
+				 {
+					 if(status.contentEquals("pause"))
+					 {
+						 pause = true;
+						 setTime = currentClip.getMicrosecondPosition();
+						 currentClip.stop();
+						 
+						 while(currentClip.getMicrosecondLength() < setTime )
+						 {
+							 setTime = setTime - currentClip.getMicrosecondLength();
+						 }
+						 
+						 
+					 }
+				 }
+	
+				 
+				 if(status.contentEquals("stop"))
 				 {
 					 System.out.println("Goodbye... ");
 					 currentClip.stop();
 					 run = false;
 
 				 }
-				 else
+				 if(pause == true)
 				 {
-					 System.out.println("Invalid Input. ");
+					 if(status.contentEquals("play"))
+					 {
+						currentClip = playSoundAtTime(fileName, setTime);
+						 pause = false;
+					 }
+				 }
+				 else if(status.contentEquals("play")) System.out.println("Already playing... ");
+				 if(!status.contentEquals("pause") && !status.contentEquals("stop") && !status.contentEquals("play"))
+				 {
+					 System.out.println("Invalid Input... ");
 				 }
 
 			 } 
+			 file.close();
+			 running.close();
+			 currentClip.close();
 	   }
-}
+	   }
+
+	 	  
+
